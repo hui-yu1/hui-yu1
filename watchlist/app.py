395d0da658e648dcc745aea3e3f4b32c23fb5f5d
lion <3478474830@qq.com>
2020-04-02 14:50:33 +0800

    数据库

--- watchlist/app.py
+++ watchlist/app.py
@@ -1,8 +1,33 @@
+import os
 
 from flask import Flask,url_for,render_template
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = "sqlite:///"  # windows平台
+else:
+    prefix = "sqlite:////"  # Mac,Linux平台
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + "sqlite:///" + os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# models 数据库
+class User(db.model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(30))
+
+
+# views视图函数
 @app.route('/')
 def index():
     name = 'lion'
