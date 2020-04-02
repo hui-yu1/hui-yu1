b97e80e469a3ac3faab5e2e51c8e798646ee6979
lion <3478474830@qq.com>
2020-04-02 09:54:16 +0800

    前端模板

--- watchlist/app.py
+++ watchlist/app.py
@@ -1,8 +1,16 @@
 
-from flask import Flask
+from flask import Flask,url_for,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>哦可</h1>'
+    name = 'lion'
+    movies = [
+        {"title":"萨霍","year":"2019"},
+        {"title":"星游记","year":"2020"},
+        {"title":"急速备战","year":"2019"},
+        {"title":"叶问4","year":"2019"},
+        {"title":"三人行","year":"2016"},
+    ]
+    return render_template('index.html',name=name,movies=movies)
