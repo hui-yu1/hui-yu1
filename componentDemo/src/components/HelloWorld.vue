f05d8effaa398f871654e825a56e8c6d2ff857f8
huiyu <3478474830@qq.com>
2020-04-14 14:53:16 +0800

    父子组件传值、组件嵌套

---
+++ componentDemo/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template id="hello">
+    <h2>{{msg}}</h2>
+
+</template>
+
+<script>
+
+Vue.component("hello-component",{
+  el:function(){
+    return "#hello"
+  },
+  data:function(){
+    return{
+      msg:'ok'
+    }
+  }
+})
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
